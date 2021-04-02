b7e81df137eb293c87b8a88aaff93ab1f4067ab6
RuefulMage <feonorabd2000@gmail.com>
2021-04-02 16:09:43 +0300

    Updates

--- src/Components/Logo/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import { logoStyles } from './Logo.styles';
-
-function Logo() {
-  const styles = logoStyles();
-
-  return (<span className={styles.root}></span>);
-}
-
-export { Logo };
--- src/Components/SocialNetworkIcon/SocialNetworkIcon.jsx
+++
@@ -1,9 +0,0 @@
-import { socialNetworkIconStyles } from './SocialNetworkIcon.styles';
-
-function SocialNetworkIcon({text, link}) {
-  const styles = socialNetworkIconStyles();
-
-  return <a className={styles.root} href={link} target="_blank" rel="noopener noreferrer">{text}</a>;
-}
-
-export { SocialNetworkIcon };
