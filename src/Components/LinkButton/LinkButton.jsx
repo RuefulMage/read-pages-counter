b528a94cc27077a1e3edf85b9a622bd542616097
RuefulMage <feonorabd2000@gmail.com>
2021-04-02 17:49:35 +0300

    link button added

---
+++ src/Components/LinkButton/LinkButton.jsx
@@ -0,0 +1,11 @@
+import { linkButtonStyles } from './LinkButton.styles';
+
+function LinkButton({text, link}) {
+  const styles = linkButtonStyles();
+
+  return (<div className={styles.root}>
+    <a className={styles.content} href={link}>{text}</a>
+  </div>);
+}
+
+export { LinkButton };
---
+++ src/Components/LinkButton/LinkButton.styles.js
@@ -0,0 +1,31 @@
+import { makeStyles } from '@material-ui/styles';
+
+const linkButtonStyles = makeStyles({
+  root: {
+    width: 'max-content',
+    padding: '2px',
+    background: 'linear-gradient(180deg, #BC9CFF 0%, #8BA4F9 100%)',
+    borderRadius: '22px',
+    outline: 'none',
+    cursor: 'pointer',
+    userSelect: 'none',
+
+    '&:focus, &:hover': {
+      background: 'linear-gradient(180deg,rgba(188,156,255,.5),rgba(139,164,249,.5))',
+    }
+  },
+  content: {
+    display: 'flex',
+    justifyContent: 'center',
+    padding: '9px 18px',
+    color: '#BC9CFF',
+    fontWeight: 'bold',
+    fontSize: '.75rem', //12px при базовом размере шрифта 16px
+    textTransform: 'uppercase',
+    textDecoration: 'none',
+    background: 'white',
+    borderRadius: 'inherit',
+  }
+});
+
+export { linkButtonStyles };
